6cf7a8c367c8438f9a3ccdf79638bdf68e4924da
sanjeev <gurungsanjeev91@gmail.com>
2024-12-07 19:39:36 +0545

    added redux practice set

---
+++ redux/src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = ({counter}) => {
+  return (
+    <div>
+      hello i am navbar and the counter is{counter}
+    </div>
+  )
+}
+
+export default Navbar
